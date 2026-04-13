d0ad8b61ed651bf2fe29f39181cc208b7b36827a
Vani Priya <priyakilaparthi36@gmail.com>
2026-04-13 19:00:43 +0530

    Create SmartDeliveryApplication.java

---
+++ SmartDeliveryApplication.java
@@ -0,0 +1,11 @@
+package com.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDeliveryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartDeliveryApplication.class, args);
+    }
+}
